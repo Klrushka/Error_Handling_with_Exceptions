f50f13c715759a5512aa86d3380e32623051d1ce
Klrushka <comeindonotbeshy@gmail.com>
2021-07-24 15:28:35 +0300

    edited

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,44 @@
+import task1.ClassWithException;
+import task2.NullClass;
+
 public class Main {
     public static void main(String[] args) {
 
 
         /*
-        Exercise 1: (2) Create a class with a main( ) that throws an object of class Exception
+        Exercise 1: (2) Create a class with a main() that throws an object of class Exception
         inside a try block. Give the constructor for Exception a String argument. Catch the
         exception inside a catch clause and print the String argument. Add a finally clause and
         print a message to prove you were there.
          */
 
+        try {
+            ClassWithException.ex();
+        } catch (Exception e) {
+            System.out.println("Caught it!");
+            e.printStackTrace();
+        } finally {
+            System.out.println("Finally clause");
+        }
+
+
+
+        /*
+        Exercise 2: (1) Define an object reference and initialize it to null. Try to call a method
+        through this reference. Now wrap the code in a try-catch clause to catch the exception.
+         */
+        NullClass nullClass = null;
+
+        try {
+            System.out.println(nullClass.toString());
+        } catch (NullPointerException e) {
+            System.out.println("Have exception");
+            e.printStackTrace();
+        }
+
+        /*
+
+         */
 
 
     }
--- src/task1/ClassWithException.java
+++ src/task1/ClassWithException.java
@@ -1,5 +1,14 @@
 package task1;
 
-public class ClassWithException {
+public class ClassWithException extends Exception {
+    ClassWithException(String str) {
+        System.out.println(str);
+    }
+
+    public static void ex() throws ClassWithException {
+        System.out.println("Throwing exception");
+        throw new ClassWithException("Exception string");
+    }
+
 
 }
